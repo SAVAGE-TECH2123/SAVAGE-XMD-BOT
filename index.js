f8dce920c093bd2c92a2a3a310cde03faac92c8e
SAVAGE-TECH2123 <SAVAGEXMD2123@gmail.com>
2025-06-22 10:52:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// index.js
+const connectToWhatsApp = require('./core/connect');
+const messageHandler = require('./handlers/messageHandler');
 
+async function startBot() {
+  try {
+    const sock = await connectToWhatsApp();
+
+    sock.ev.on('messages.upsert', async (msg) => {
+      await messageHandler(sock, msg);
+    });
+
+    console.log('✅ SAVAGE-XMD is now online!');
+  } catch (err) {
+    console.error('❌ Bot startup failed:', err);
+  }
+}
+
+startBot();
