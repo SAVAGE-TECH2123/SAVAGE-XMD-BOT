9f5265f683cbcf2246112735467f188f63760a2e
SAVAGE-TECH2123 <SAVAGEXMD2123@gmail.com>
2025-06-23 11:59:50 +0300

    Update and rename connect.js to core/connect.js

--- connect.js
+++
@@ -1,7 +0,0 @@
-// index.js
-require('dotenv').config();
-const connectToWhatsApp = require('./connect');
-
-connectToWhatsApp()
-  .then(() => console.log('✅ SAVAGE-XMD bot started'))
-  .catch((err) => console.error('❌ Error starting bot:', err));
